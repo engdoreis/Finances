c4571760a69fb09addb9276889b3c0f8f90465c1
Douglas Reis <doreis@lowrisc.org>
2022-09-19 14:06:34 +0100

    Refactor the BroakerParser regex
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- BroakerParser.py
+++ BroakerParser.py
@@ -27,16 +27,6 @@ class Broaker():
 
 class Clear(Broaker):
     def __init__(self, outDir, name='default'):
-        self.date_re = re.compile(r'\d{2}/\d{2}/\d{4}$')
-        self.operation_re = re.compile(r'[\w\d-]+\s(C|V)\s+(?:VISTA|FRACIONARIO)\s(?:\d\d\/\d\d)?([\w\d\s.\/]+?)\s\s+([\w\d\s#]+?)\s([\d.,]+)\s([\d.,]+)\s([\d.,]+)\s(\w)')
-        self.liqFee_re = re.compile(r'.*?Taxa de liquida.*?\s+([\d,]+)')
-        self.emolFee_re = re.compile(r'Emolumentos\s+([\d,]+)')
-        self.opFee_re = re.compile(r'Taxa Operacional\s+([\d,]+)')
-        self.exFee_re = re.compile(r'Execu\w+\s+([\d,]+)')
-        self.custodyFee_re = re.compile(r'.*?Taxa de Cust\w+\s+([\d,]+)')
-        self.irrf_re = re.compile(r'I.R.R.F.*?base.*?[\d,]+\s([\d,]+)')
-        self.taxes_re = re.compile(r'Impostos\s+([\d,]+)')
-        self.otherFee_re = re.compile(r'Outros\s+([\d,]+)')
         super().__init__(outDir, name)
 
     def process(self, page):
@@ -46,7 +36,8 @@ class Clear(Broaker):
         order = namedtuple('order', 'Code Date Company Type Category Qty Value Total Sub ')
         line_itens = []
         for line in text.split('\n'):
-            res = self.operation_re.search(line)
+            res = re.compile(r'[\w\d-]+\s(C|V)\s+(?:VISTA|FRACIONARIO)\s(?:\d\d\/\d\d)?([\w\d\s.\/]+?)\s\s+([\w\d\s#]+?)\s([\d.,]+)\s([\d.,]+)\s([\d.,]+)\s(\w)')\
+                    .search(line)
             if res:
                 # print (res.group(0))
                 opType = 'S' if res.group(1)=='V' else 'B'
@@ -59,50 +50,51 @@ class Clear(Broaker):
                 else:
                     category = 'Ação'
 
-                line_itens.append(order(code, Date, name, opType, category, int(res.group(4).replace('.','')), to_float(res.group(5)), to_float(res.group(6)), code.split(' ')[0] ))
+                line_itens.append(order(code, Date, name, opType, category,\
+                     int(res.group(4).replace('.','')), to_float(res.group(5)), to_float(res.group(6)), code.split(' ')[0] ))
                 continue
 
-            res = self.date_re.search(line)
+            res = re.compile(r'\d{2}/\d{2}/\d{4}$').search(line)
             if res:
                 Date = pd.to_datetime(res.group(0), format='%d/%m/%Y').strftime('%Y-%m-%d')
                 continue
 
-            res = self.liqFee_re.search(line)
+            res = re.compile(r'.*?Taxa de liquida.*?\s+([\d,]+)').search(line)
             if res:
                 liqFee = to_float(res.group(1))
                 continue
 
-            res = self.emolFee_re.search(line)
+            res = re.compile(r'Emolumentos\s+([\d,]+)').search(line)
             if res:
                 emolFee =to_float( res.group(1))
                 continue
             
-            res = self.opFee_re.search(line)
+            res = re.compile(r'Taxa Operacional\s+([\d,]+)').search(line)
             if res:
                 opFee =to_float( res.group(1))
                 continue
             
-            res = self.exFee_re.search(line)
+            res = re.compile(r'Execu\w+\s+([\d,]+)').search(line)
             if res:
                 exFee =to_float( res.group(1))
                 continue
 
-            res = self.custodyFee_re.search(line)
+            res = re.compile(r'.*?Taxa de Cust\w+\s+([\d,]+)').search(line)
             if res:
                 custodyFee =to_float( res.group(1))
                 continue
 
-            res = self.irrf_re.search(line)
+            res = re.compile(r'I.R.R.F.*?base.*?[\d,]+\s([\d,]+)').search(line)
             if res:
                 irrf = to_float( res.group(1))
                 continue
 
-            res = self.taxes_re.search(line)
+            res = re.compile(r'Impostos\s+([\d,]+)').search(line)
             if res:
                 taxes = to_float( res.group(1))
                 continue
 
-            res = self.otherFee_re.search(line)
+            res = re.compile(r'Outros\s+([\d,]+)').search(line)
             if res:
                 otherFee = to_float( res.group(1))
                 continue
@@ -124,9 +116,6 @@ class Clear(Broaker):
 class TDAmeritrade(Broaker):
     def __init__(self, outDir, name='default'):
         self.output = outDir + '/' + name + '.csv'
-        self.operation_re = re.compile(r'YOU\s(BOUGHT|SOLD)\s+(\d+)\s+.+?\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)')
-        self.date_re = re.compile(r'(\d{2}\/\d{2}\/\d{4})\s+(\d{2}\/\d{2}\/\d{4})\s+([\d.]+)\s+([\d.]+)')
-        self.Ticker_re = re.compile(r'^\s(\w+)\s\s\w+(\s\w+)?$')
         super().__init__(outDir, name)
         
     def process(self, page):
@@ -135,7 +124,7 @@ class TDAmeritrade(Broaker):
         order = namedtuple('order', 'Code Date Company Type Category Qty Value Total Sub Fee')
         line_itens = []
         for line in text.split('\n'):
-            res = self.operation_re.search(line)
+            res = re.compile(r'YOU\s(BOUGHT|SOLD)\s+(\d+)\s+.+?\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)').search(line)
             if res:
                 # print (res.group(0))
                 opType = 'B' if res.group(1) == 'BOUGHT' else 'S'
@@ -144,14 +133,14 @@ class TDAmeritrade(Broaker):
                 fee = float(res.group(5))
                 continue
 
-            res = self.date_re.search(line)
+            res = re.compile(r'(\d{2}\/\d{2}\/\d{4})\s+(\d{2}\/\d{2}\/\d{4})\s+([\d.]+)\s+([\d.]+)').search(line)
             if res:
                 # print (res.group(0))
                 date = pd.to_datetime(res.group(1), format='%m/%d/%Y').strftime('%Y-%m-%d')
                 total = res.group(4)
                 continue
 
-            res = self.Ticker_re.search(line)
+            res = re.compile(r'^\s(\w+)\s\s\w+(\s\w+)?$').search(line)
             if res:
                 # print (res.group(0))
                 line_itens.append(order(res.group(1), date, 'Company', opType, 'Stock', qty, value, total,'sub',fee))
@@ -164,15 +153,13 @@ class Clear_proventos():
         self.dtFrame = pd.DataFrame(columns=list('Ativo Evento Quantidade ValorBruto ValorIR ValorLiquido Pagamento'.split()))
         self.dividend_re = re.compile(r'DIVIDENDOS\s([\d]+)\s')
 
-        self.single_evt = re.compile(r'([a-zA-Z/. ]+)\s([a-zA-Z/]+)\s([0-9,.]+)\s([0-9,.]+)\s([0-9,.]+)\s([0-9,.]+)\s([0-9/]+)')
-        
     def process(self, page):
         text = page.extract_text()
 
         order = namedtuple('order', 'Ativo Evento Quantidade ValorBruto ValorIR ValorLiquido Pagamento')
         line_itens = []
         for line in text.split('\n'):
-            res = self.single_evt.search(line)
+            res = re.compile(r'([a-zA-Z/. ]+)\s([a-zA-Z/]+)\s([0-9,.]+)\s([0-9,.]+)\s([0-9,.]+)\s([0-9,.]+)\s([0-9/]+)').search(line)
             if res:
                 # print (res.group(0))
                 line_itens.append(order(res.group(1),res.group(2), res.group(3), res.group(4), res.group(5), res.group(6), res.group(7)))
