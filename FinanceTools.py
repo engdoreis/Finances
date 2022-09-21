d3e42bc9ddde5bd94021be29e4549817949319f2
Douglas Reis <doreis@lowrisc.org>
2022-09-21 12:47:28 +0100

    add indices comparisson to the PerformanceViewer
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- FinanceTools.py
+++ FinanceTools.py
@@ -617,12 +617,14 @@ class PerformanceViewer:
         self.pf = pd.DataFrame(columns = ['Item', 'BRL', 'USD', '%'])
         if (len(args) == 2 and isinstance(args[0], pd.DataFrame)):
             row = args[0].set_index('Date').loc[args[1]]
-            self.buildTable(row['Equity'], row['Cost'], row['Expense'], row['paperProfit'], row['Profit'], row['Div'], row['TotalProfit'])
+            self.buildTable(row['Equity'], row['Cost'], row['Expense'], row['paperProfit'], row['Profit'], row['Div'], row['TotalProfit'],\
+                            row['selic'], row['Ibov'], row['SP500'])
         elif(isinstance(args[0], PerformanceBlueprint)):
             p = args[0]
-            self.buildTable(p.equity, p.cost, p.expense, p.paperProfit, p.realizedProfit, p.div, p.profit, p.currency, p.exchangeRatio)
+            self.buildTable(p.equity, p.cost, p.expense, p.paperProfit, p.realizedProfit, p.div, p.profit,\
+                            p.cum_cdb, p.ibov, p.sp500, p.currency, p.exchangeRatio)
 
-    def buildTable(self, equity, cost, expense, paperProfit, profit, div, totalProfit, currency='$', exchangeRatio=0.22):
+    def buildTable(self, equity, cost, expense, paperProfit, profit, div, totalProfit, selic, ibov, sp500, currency='$', exchangeRatio=0.22):
         self.pf.loc[len(self.pf)] = ['Equity          ' , equity,equity, equity/cost]
         self.pf.loc[len(self.pf)] = ['Cost            ' , cost,cost, 1]
         self.pf.loc[len(self.pf)] = ['Expenses        ' , expense,expense, expense/cost]
@@ -630,11 +632,14 @@ class PerformanceViewer:
         self.pf.loc[len(self.pf)] = ['Realized profit ' , profit,profit, profit/cost]
         self.pf.loc[len(self.pf)] = ['Dividends       ' , div,div, div/cost]
         self.pf.loc[len(self.pf)] = ['Total Profit    ' , totalProfit,totalProfit, totalProfit/cost]
-        self.pf.loc[:, '%'] *= 100
         if currency == '$':
             self.pf.loc[:, 'BRL'] /= exchangeRatio
         else:
             self.pf.loc[:, 'USD'] *= exchangeRatio
+        self.pf.loc[len(self.pf)] = ['Selic    ' , 0,0, selic]
+        self.pf.loc[len(self.pf)] = ['Ibov     ' , 0,0, ibov]
+        self.pf.loc[len(self.pf)] = ['S&P500   ' , 0,0, sp500]
+        self.pf.loc[:, '%'] *= 100
         self.pf.set_index('Item', inplace=True)
 
     def show(self):
